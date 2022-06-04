5a9abe57c949b80b61dc6c4a4abffc3675a1f736
JuanJoseTJ29 <juan.tirado@unmsm.edu.pe>
2022-06-03 19:48:28 -0500

    Interfaz de una incidencia

--- lib/main.dart
+++ lib/main.dart
@@ -1,45 +1,25 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
-
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
+      title: 'INTERFAZ DE INCIDENCIA',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
         primarySwatch: Colors.blue,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const MyHomePage(title: 'INTERFAZ DE INCIDENCIA'),
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
   const MyHomePage({Key? key, required this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
 
   final String title;
 
@@ -48,68 +28,102 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
-
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
         title: Text(widget.title),
       ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
+      body: SafeArea(
+        child: GestureDetector(
+          onTap: () => FocusScope.of(context).unfocus(),
+          child: Column(
+            mainAxisSize: MainAxisSize.max,
+            children: [
+              Padding(
+                padding: EdgeInsets.all(5.0),
+              ),
+              Align(
+                alignment: AlignmentDirectional(-0.98, 0.1),
+                child: Text('FALTAN 5 CARPETAS URGENTE!',
+                    style: TextStyle(
+                        color: Color.fromARGB(255, 18, 183, 43),
+                        fontWeight: FontWeight.w800)),
+              ),
+              Padding(
+                padding: EdgeInsets.all(5.0),
+              ),
+              Align(
+                alignment: AlignmentDirectional(-0.94, 0),
+                child: Text('Facultad de letras',
+                    textAlign: TextAlign.start,
+                    style: TextStyle(
+                        color: Colors.black, fontWeight: FontWeight.w800)),
+              ),
+              Align(
+                alignment: AlignmentDirectional(-0.05, -0.05),
+                child: Image.asset(
+                  'assets/Carpetas.jpg',
+                  width: 200,
+                  height: 180,
+                  fit: BoxFit.cover,
+                ),
+              ),
+              Text(
+                'Descripción:',
+                textAlign: TextAlign.justify,
+                style: TextStyle(
+                  color: Colors.blue,
+                ),
+              ),
+              Padding(
+                padding: EdgeInsets.all(5.0),
+              ),
+              Text(
+                'Faltan 5 carpetas en el salon 2A\nde la facultad de Letras, por favor\ntraerlo los mas rapido posible ya \nque hay alumnos sin asiento.',
+                textAlign: TextAlign.justify,
+              ),
+              Padding(
+                padding: EdgeInsets.all(10.0),
+              ),
+              Align(
+                alignment: AlignmentDirectional(-0.75, 0),
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  ElevatedButton(
+                    child: Text(
+                      'Editar',
+                    ),
+                    style: ElevatedButton.styleFrom(
+                      primary: Color.fromARGB(255, 31, 115, 241),
+                    ),
+                    onPressed: () {},
+                  ),
+                  ElevatedButton(
+                    child: Text(
+                      'Eliminar',
+                    ),
+                    style: ElevatedButton.styleFrom(
+                        primary: Color.fromARGB(255, 243, 12, 12)),
+                    onPressed: () {},
+                  ),
+                ],
+              ),
+              ElevatedButton(
+                child: Text(
+                  'Corregido',
+                ),
+                style: ElevatedButton.styleFrom(
+                    primary: Color.fromARGB(255, 9, 229, 16)),
+                onPressed: () {},
+              ),
+            ],
+          ),
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
