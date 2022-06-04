ca7cd383908e612867c1c586852598b5f6770654
unknown <rodrigoqp0722@gmail.com>
2022-06-04 10:07:37 -0500

    Lista de incidencias

---
+++ lib/incidents/incidents_list.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+
+import 'incidents.dart';
+
+class ReviewList extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    //Retornamos la columna
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: <Widget>[
+        Incident("assets/images/people_1.jpeg", "Perdida de Carnet",
+            "Varuna Yasas", "Lorem Ipsum is simply dummy text"),
+        Incident(
+            "assets/images/people_2.jpg",
+            "Computadora sin funcionamiento",
+            "Anahí Salgado",
+            "Lorem Ipsum is simply dummy text of the printing"),
+        Incident(
+            "assets/images/people_3.jpg",
+            "Perdida de Carnet",
+            "Gissele Thomas",
+            "Lorem Ipsum is simply dummy text of the printing"),
+        Incident("assets/images/people_4.jpg", "Perdida de Carnet",
+            "Varuna Yasas", "Lorem Ipsum is simply dummy text of the printing"),
+        Incident(
+            "assets/images/people_5.jpg",
+            "Facultad sin Luz",
+            "Anahí Salgado",
+            "Lorem Ipsum is simply dummy text of the printing"),
+        Incident(
+            "assets/images/people_6.jpg",
+            "Facultad sin Agua",
+            "Gissele Thomas",
+            "Lorem Ipsum is simply dummy text of the printing"),
+      ],
+    );
+  }
+}
