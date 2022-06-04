067211d1c8a2fa5ae4dfa76489354f81da31e4e0
SebasRodz <sebasxiom@gmail.com>
2022-06-04 11:24:51 -0500

    Agregando validaciones

--- lib/main.dart
+++ lib/main.dart
@@ -2,63 +2,99 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(new MyApp());
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
   @override
-  Widget build(BuildContext context)
-  {
+  Widget build(BuildContext context) {
+    final titulo = "Encuesta";
     return MaterialApp(
         debugShowCheckedModeBanner: false,
         home: Scaffold(
           appBar: AppBar(
               centerTitle: true,
-              title: Text("Encuesta", style: TextStyle(color: Colors.black)),
-              backgroundColor: Colors.white
-          ),
-          body: Form(
-            child: (
-                Column(
-                  children: <Widget> [
-                    SizedBox(height: 20),
-                    TextFormField(
-                        maxLines: 3,
-                        decoration: InputDecoration(
-                            border: OutlineInputBorder(),
-                            labelText: 'Encuesta : ')
-                    ),
-                    SizedBox(height: 20),
-                    TextFormField(
-                      maxLines: 3,
-                      decoration: InputDecoration(border: OutlineInputBorder(),
-                          labelText: 'Comentario : '),
-                    ),
-                    SizedBox(height: 20),
-                    Center(
-                        child: (
-                            Row (
-                                mainAxisAlignment: MainAxisAlignment.center,
-                                children: <Widget> [
-                                  ElevatedButton(
-                                    onPressed: (){ },
-                                    child: Text('Enviar', style: TextStyle(fontSize: 20)),
-                                  ),
-                                  SizedBox(width: 80),
-                                  ElevatedButton(
-                                    onPressed: (){ },
-                                    child: Text('Cancelar', style: TextStyle(color: Colors.white, fontSize: 20)),
-                                    style: ElevatedButton.styleFrom(primary: Colors.red),
-                                  )
-                                ]
-                            )
-                        )
-                    )
-                  ],
+              title: Text(titulo, style: TextStyle(color: Colors.black)),
+              backgroundColor: Colors.white),
+          body: Encuesta(),
+        ),
+    );
+  }
+}
+
+class Encuesta extends StatefulWidget {
+  @override
+  State< Encuesta> createState() => _EncuestaState();
+}
+
+class _EncuestaState extends State< Encuesta> {
+  static final RegExp _comentarioExp = RegExp(
+      r"^[a-zA-Z\s]*$"
+  );
+
+  final _formkey = GlobalKey<FormState>();
+
+  bool _esComentario(String str) {
+    return _comentarioExp.hasMatch(str.toLowerCase());
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Form(
+      key: _formkey,
+      child: (Column(
+        children: <Widget>[
+          SizedBox(height: 20),
+          TextFormField(
+              maxLines: 3,
+              decoration: InputDecoration(
+                  border: OutlineInputBorder(), labelText: 'Encuesta : '),
+              validator: (value) {
+                if (value!.isEmpty) {
+                  return "Ingrese algun caracter";
+                }
+                //else {
+                  //  Aqui irian los de la encuesta
+                //}
+              }),
+          SizedBox(height: 20),
+          TextFormField(
+              maxLines: 3,
+              decoration: InputDecoration(
+                  border: OutlineInputBorder(), labelText: 'Comentario : '),
+              validator: (value) {
+                if (value!.isEmpty) {
+                  return "Ingrese algun caracter";
+                } else {
+                  if (!_esComentario(value.toString())) {
+                    return 'Ingrese solo letras y espacios';
+                  }
+                }
+              }),
+          SizedBox(height: 20),
+          Center(
+              child: (Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                ElevatedButton(
+                  onPressed: () {
+                    if (_formkey.currentState!.validate()) {
+                      Scaffold.of(context).showSnackBar(
+                          SnackBar(content: Text("Procesando datos...")));
+                    }
+                  },
+                  child: Text('Enviar', style: TextStyle(fontSize: 20)),
+                ),
+                SizedBox(width: 80),
+                ElevatedButton(
+                  onPressed: () {},
+                  child: Text('Cancelar',
+                      style: TextStyle(color: Colors.white, fontSize: 20)),
+                  style: ElevatedButton.styleFrom(primary: Colors.red),
                 )
-            ),
-          ),
-        )
+              ])))
+        ],
+      )),
     );
   }
 }
